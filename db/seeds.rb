3731be495f3b255f1a7c38f9a4fab7394e2c6d25
Ross Grayton <rossgrayton@gmail.com>
2012-01-29 16:23:43 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(:email => 'test@example.com', :password => 'password')
+board = Board.create(:title => 'Board 1', :description => 'An example board', :created_by => user)
+
+Lane.create([
+  { :title => 'Backlog', :board => board },
+  { :title => 'In Progress', :board => board },
+  { :title => 'Done', :board => board }
+])
+
+Card.create([
+  { :title => 'Card 1', :lane => Lane.find_by_title('Backlog') },
+  { :title => 'Card 2', :lane => Lane.find_by_title('Backlog') },
+  { :title => 'Card 3', :lane => Lane.find_by_title('Backlog') },
+  { :title => 'Card 4', :lane => Lane.find_by_title('In Progress') },
+  { :title => 'Card 5', :lane => Lane.find_by_title('In Progress') },
+  { :title => 'Card 6', :lane => Lane.find_by_title('Done') }
+])
